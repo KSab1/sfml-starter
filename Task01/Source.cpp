9b4dda20c83e66f4bc5038df59b3f0a548c32cb8
office11 <office11@ucione.local>
2025-04-18 12:50:15 +0200

    movement completed

--- Task01/Source.cpp
+++ Task01/Source.cpp
@@ -1,10 +1,18 @@
 #include <SFML/Graphics.hpp>
 
+static void MoveShape(float xMove, float yMove);
+
+float x = 0, y = 0;
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "This is snake!!!!");
+
+    float step = 0.2;
+
+    sf::CircleShape shape(50.f);
     shape.setFillColor(sf::Color::Green);
+    shape.setPosition({ x, y });
 
     while (window.isOpen())
     {
@@ -14,8 +22,33 @@ int main()
                 window.close();
         }
 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+        {
+            MoveShape(-step, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+        {
+            MoveShape(0, step);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+        {
+            MoveShape(step, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+        {
+            MoveShape(0, -step);
+        }
+
+        shape.setPosition({ x, y });
+
         window.clear();
         window.draw(shape);
         window.display();
     }
+}
+
+static void MoveShape(float xMove, float yMove)
+{
+    x += xMove;
+    y += yMove;
 }
